1f5debc7c5e5bdd0728876bc2fe4caa9d2a90a45
MksMil <maksymmiliaiev@gmail.com>
2023-09-28 09:38:13 +0300

    Initial Commit

---
+++ BroadcastPlanner/BroadcastPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroadcastPlannerApp.swift
+//  BroadcastPlanner
+//
+//  Created by Миляев Максим on 28.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BroadcastPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BroadcastPlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BroadcastPlanner
+//
+//  Created by Миляев Максим on 28.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
