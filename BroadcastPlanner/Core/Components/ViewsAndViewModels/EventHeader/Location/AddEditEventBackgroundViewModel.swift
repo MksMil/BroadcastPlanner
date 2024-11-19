6aabfd1b5f8f7a62c9b9bf951386891090ea151a
MksMil <maksymmiliaiev@gmail.com>
2024-11-19 20:32:40 +0200

    improve perfomance in eventHeader section

---
+++ BroadcastPlanner/Core/Components/ViewsAndViewModels/EventHeader/Location/AddEditEventBackgroundViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddEditEventBackgroundViewModel.swift
+//  BroadcastPlanner
+//
+//  Created by Миляев Максим on 19.11.2024.
+//
+
+import Foundation
---
+++ BroadcastPlanner/Core/Components/ViewsAndViewModels/EventHeader/Location/CellImage.swift
@@ -0,0 +1,18 @@
+//
+//  CellImage.swift
+//  BroadcastPlanner
+//
+//  Created by Миляев Максим on 19.11.2024.
+//
+
+import SwiftUI
+
+struct CellImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CellImage()
+}
